d119a3e880b4f2427c60b9b9c0d0599e1da940d0
HeitorPD25 <heitorpdte@gmail.com>
2025-04-04 20:45:31 -0300

    - Anotação "@OpenAPIDefinition", na classe "App.java", para definir informações no Swagger.

--- src/main/java/br/com/fiap/bibliothecca_api/App.java
+++ src/main/java/br/com/fiap/bibliothecca_api/App.java
@@ -2,8 +2,20 @@ package br.com.fiap.bibliothecca_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+	info = @Info(
+		title = "Bibliotheca API",
+		version = "1.0",
+		description = "API para gerenciamento de livros"
+	)
+)
+@EnableCaching
 public class App {
 
 	public static void main(String[] args) {
